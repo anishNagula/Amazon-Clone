184395e001255d29cea55ea4a3cc4d33cb2fdac7
anishNagula <anish.nagula@gmail.com>
2024-10-07 22:57:16 +0530

    uploading files to git

---
+++ tests/tests-simple/moneyTest.js
@@ -0,0 +1,24 @@
+import {formatCurrency} from "../../scripts/utils/money.js";
+
+console.log('test suite: formart currency');
+
+console.log('Converts cents into dollars');
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with 0');
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
